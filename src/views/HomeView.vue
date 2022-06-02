19f6bd215fafae5d986aa28743e107b7047a2150
apachqq <work.tsyganov@mail.ru>
2022-06-02 13:11:58 +0300

    navigate-btn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,25 @@
     <div class="card">
         <h1>{{ title }}</h1>
 
-        <button class="btn">Открыть пользователей</button>
+        <button class="btn" @click="navigate">Открыть пользователей</button>
     </div>
 </template>
 
 <script lang="ts">
     import {defineComponent} from 'vue'
+    import {useRouter} from 'vue-router'
 
     export default defineComponent({
         setup() {
+            const router = useRouter()
+
+            const navigate = (): void => {
+                router.push('/users')
+            }
+
             return {
-                title: 'Как Vue работает с TypeScript?'
+                title: 'Как Vue работает с TypeScript?',
+                navigate
             }
         }
         // data() {
