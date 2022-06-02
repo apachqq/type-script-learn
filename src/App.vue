9467edf951d224c2f43a6ccd8eb94d8e0487296d
apachqq <work.tsyganov@mail.ru>
2022-06-02 17:23:48 +0300

    vuex-with-ts

--- src/App.vue
+++ src/App.vue
@@ -14,3 +14,15 @@
         <router-view></router-view>
     </main>
 </template>
+
+<script lang="ts">
+    import {defineComponent} from 'vue'
+
+    export default defineComponent({
+        methods: {
+            workWithStore() {
+                this.$store.state
+            }
+        }
+    })
+</script>
